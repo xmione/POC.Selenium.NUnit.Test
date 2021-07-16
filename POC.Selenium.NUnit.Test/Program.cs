ce4fba61549e16f20878e0908bb39ba4ff01274d
Solomio Sisante <ssisante@timelio.com.au>
2021-07-16 09:49:30 +0800

    Add project files.

---
+++ POC.Selenium.NUnit.Test/Program.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace POC.Selenium.NUnit.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www.demoqa.com";
+        }
+    }
+}
