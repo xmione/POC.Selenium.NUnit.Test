6501399aed9ad73a5ae93db2c2b559556f79ca1a
Solomio Sisante <ssisante@timelio.com.au>
2021-07-16 09:57:18 +0800

    Added TestCase1

---
+++ POC.Selenium.NUnit.Test/TestCase1.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POC.Selenium.NUnit.Test
+{
+    class TestCase1
+    {
+        IWebDriver driver;
+        
+        [SetUp]
+        public void Initialize()
+        {
+            driver = new ChromeDriver();
+        }
+        
+        [Test]
+        public void OpenAppTest()
+        {
+            driver.Url = "https://www.demoqa.com";
+        }
+
+        [TearDown]
+        public void EndTest()
+        {
+            driver.Close();
+        }
+    }
+}
